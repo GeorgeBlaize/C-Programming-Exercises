0328ff05e159835e0479206b7400b193381fa2ac
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-17 17:23:51 +0600

    Add files via upload

---
+++ Exercise-1/Problem-17.c
@@ -0,0 +1,22 @@
+
+//C Program to Calculate the Sum of Natural Numbers
+
+#include<stdio.h>
+
+int main()
+{
+    int num,sum=0;
+
+    printf("Enter a positive integer: ");
+
+    scanf("%d",&num);
+
+    for(int i=0; i<=num; i++)
+    {
+        sum+=i;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
