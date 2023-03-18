f44a5f68b6ffa4166c7438c5722e00a058dbd9cc
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-18 20:15:50 +0600

    Add files via upload

---
+++ Exercise-1/Problem-24.c
@@ -0,0 +1,23 @@
+
+//C Program to Count Number of Digits in an Integer
+
+#include<stdio.h>
+
+int main()
+{
+
+    int num,count=0;
+
+    printf("Enter an integer: ");
+    scanf("%d",&num);
+
+    while(num!=0)
+    {
+        num=num/10;
+        count++;
+    }
+
+    printf("Number of digits: %d",count);
+
+    return 0;
+}
