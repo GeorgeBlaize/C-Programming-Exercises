f6586107c28044be6cee90f4cd7d921e6ac935bc
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-18 20:11:08 +0600

    Add files via upload

---
+++ Exercise-1/Problem-23.c
@@ -0,0 +1,17 @@
+
+//C Program to Display Characters from A to Z Using Loop
+
+#include<stdio.h>
+
+int main()
+{
+
+    char ch;
+
+    for(char ch='A'; ch<='Z'; ch++)
+    {
+        printf("%c ",ch);
+    }
+
+    return 0;
+}
