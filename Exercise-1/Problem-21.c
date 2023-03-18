75552096a9cd24a368d71735e54c6a4886848762
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-18 19:52:33 +0600

    Add files via upload

---
+++ Exercise-1/Problem-21.c
@@ -0,0 +1,30 @@
+
+//C Program to Find GCD of two Numbers
+
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,n1,n2,gcd,rem;
+
+    printf("Enter two numbers: ");
+
+    scanf("%d %d",&num1,&num2);
+
+    n1=num1;
+    n2=num2;
+
+    while(n2!=0)
+    {
+        rem=n1%n2;
+
+        n1=n2;
+
+        n2=rem;
+    }
+    gcd=n1;
+
+    printf("GCD = %d\n",gcd);
+
+    return 0;
+}
