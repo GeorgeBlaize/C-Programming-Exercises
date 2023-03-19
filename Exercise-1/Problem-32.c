1e77f5cfdb19375d310605988e127f4c11c9d347
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-19 23:11:29 +0600

    Add files via upload

---
+++ Exercise-1/Problem-32.c
@@ -0,0 +1,26 @@
+
+//C Program to Display Factors of a Number
+
+#include<stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a positive integer: ");
+
+    scanf("%d",&num);
+
+    printf("Factors of %d are: ",num);
+
+    for(int i=1; i<=num; i++)
+    {
+        if(num%i==0)
+        {
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+
+}
