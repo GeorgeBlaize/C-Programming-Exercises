e10fa5a86143b700b534c798f81d2f7b0e5ba934
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-03-21 20:12:39 +0600

    Add files via upload

---
+++ Exercise-1/Problem-44.c
@@ -0,0 +1,35 @@
+
+//C program to calculate the power using recursion
+
+#include<stdio.h>
+
+int power(int base,int exponent)
+{
+    if(exponent==0)
+    {
+        return 1;
+
+    }
+    else
+    {
+        return base*power(base,exponent-1);
+    }
+}
+int main()
+{
+    int base,exponent,result;
+
+    printf("Enter base: ");
+
+    scanf("%d",&base);
+
+    printf("Enter exponent: ");
+
+    scanf("%d",&exponent);
+
+    result= power(base,exponent);
+
+    printf("%d to the power of %d is %d\n",base,exponent,result);
+
+    return 0;
+}
