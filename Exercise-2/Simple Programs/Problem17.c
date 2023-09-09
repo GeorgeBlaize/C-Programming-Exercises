234b082b36bdfc3d175ca236f24125b9b07e0ad3
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-09-09 16:21:32 +0600

    Create Problem17.c

---
+++ Exercise-2/Simple Programs/Problem17.c
@@ -0,0 +1,16 @@
+//C Program to Print an Integer entered by the user
+
+#include<stdio.h>
+
+int main()
+{
+
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d",&num);
+
+    printf("Integer entered by user: %d",num);
+
+    return 0;
+}
