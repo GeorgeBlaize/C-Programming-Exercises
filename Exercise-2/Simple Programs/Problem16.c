89ee2213899cbcbb0e4bd611860fc4e9fd7b0a8d
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-09-09 16:14:19 +0600

    Create Problem16.c

---
+++ Exercise-2/Simple Programs/Problem16.c
@@ -0,0 +1,14 @@
+//C Program to find the Size of int, float, double and char
+
+#include<stdio.h>
+int main()
+{
+
+    printf("Size of char: %ld bytes\n",sizeof(char));
+    printf("Size of int : %ld bytes\n",sizeof(int));
+    printf("Size of float : %ld bytes\n",sizeof(float));
+    printf("Size of double : %ld bytes\n",sizeof(double));
+
+    return 0;
+
+}
