78b82c7c2331fce094586f4cf537353819ad4a4a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-09-09 16:03:44 +0600

    Create Problem13.c

---
+++ Exercise-2/Simple Programs/Problem13.c
@@ -0,0 +1,33 @@
+//C Program to find Palindrome numbers in a given range
+
+#include<stdio.h>
+int main()
+{
+
+    int num,rem,reverseNum,temp,start,end;
+
+    printf("Enter the lower limit: ");
+    scanf("%d",&start);
+    printf("Enter the upper limit: ");
+    scanf("%d",&end);
+
+    printf("Palindrome numbers between %d and %d are: ",start,end);
+    for(num=start; num<=end; num++){
+
+        temp=num;
+        reverseNum=0;
+        while(temp){
+            rem=temp%10;
+            temp=temp/10;
+            reverseNum=reverseNum*10+rem;
+        }
+
+        if(num==reverseNum){
+                printf("%d ",num);
+        }
+    }
+    return 0;
+
+
+
+}
