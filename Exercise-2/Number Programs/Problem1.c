1bc9bf68d5c142c334d684ffe473819c416e5ad2
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-09-11 00:13:12 +0600

    Create Problem1.c

---
+++ Exercise-2/Number Programs/Problem1.c
@@ -0,0 +1,16 @@
+//C Program to Add two numbers
+
+#include<stdio.h>
+int main()
+{
+
+    int num1,num2,sum;
+    printf("Enter two integers: ");
+    scanf("%d %d",&num1,&num2);
+
+    sum=num1+num2;
+
+    printf("Sum of %d and %d is : %d",num1,num2,sum);
+
+    return 0;
+}
