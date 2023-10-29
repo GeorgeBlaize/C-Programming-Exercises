2db75759b6f42e81f657f0c2e7c8e18c490a90c1
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-10-29 14:57:26 +0600

    Create Problem3.c

---
+++ Exercise-3/Problem3.c
@@ -0,0 +1,17 @@
+//Area of square
+
+#include<stdio.h>
+
+int main()
+{
+    int side,area;
+
+    printf("Enter the Length of Side:");
+    scanf("%d",&side);
+
+    area=side*side;
+
+    printf("Area of Square: %d",area);
+
+    return 0;
+}
