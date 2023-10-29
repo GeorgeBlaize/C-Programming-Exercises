6913dc2d66d1987496fb150c7a1aaa12cea5423a
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-10-29 15:29:41 +0600

    Create Problem8.c

---
+++ Exercise-3/Problem8.c
@@ -0,0 +1,17 @@
+//Addition of two numbers
+
+#include<stdio.h>
+
+int main()
+{
+
+    int first,second,sum;
+    printf("Enter two integers to add:\n");
+    scanf("%d %d",&first,&second);
+
+    sum=first+second;
+
+    printf("Sum of entered numbers= %d\n",sum);
+
+    return 0;
+}
