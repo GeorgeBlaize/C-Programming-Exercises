32b65f3a72cd304b5606a3dcaae9cd9231377a5c
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-12-22 18:53:58 +0600

    Create Problem8.c

---
+++ Exercise-4/Basic Programs/Problem8.c
@@ -0,0 +1,20 @@
+
+//Calculate area of square
+
+#include<stdio.h>
+
+int main()
+
+{
+
+
+    int side,area;
+    printf("Enter the length of side: ");
+    scanf("%d",&side);
+
+    area=side*side;
+
+    printf("Area of square: %d",area);
+
+    return 0;
+}
