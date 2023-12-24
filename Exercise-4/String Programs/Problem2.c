aaa2c55c8c4461d32c551da74970ec33c5d333e8
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2023-12-24 15:03:07 +0600

    Create Problem2.c

---
+++ Exercise-4/String Programs/Problem2.c
@@ -0,0 +1,23 @@
+
+//Count number of spaces in a string
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+    char ch[10];
+
+    int count=0;
+
+    printf("Enter the string: \n");
+    gets(ch);
+    for(int i=0; ch[i]!='\0'; i++)
+    {
+        if(ch[i]==' ')
+        count++;
+    }
+    printf("Spaces = %d",count);
+    getch();
+}
