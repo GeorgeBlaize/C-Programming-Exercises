87a3aeaa68caac7340f03889c7ee0cdc72de2cfe
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2024-01-18 02:39:28 +0600

    Create Problem5.c

---
+++ Exercise-5/Problem5.c
@@ -0,0 +1,27 @@
+
+//Armstrong Number
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int n,r,sum=0,temp;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    temp=n;
+
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+
+    if(temp==sum)
+        printf("Armstrong number");
+    else
+        printf("Not armstrong number");
+
+    getch();
+}
